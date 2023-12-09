72f14e315bfc0789e08c76b725f856bb90e0fcf9
dmitrykap1an <dk475641@gmail.com>
2023-12-09 20:58:51 +0300

    added endpoints for food

--- food-service/src/main/kotlin/com/example/foodservice/domain/entity/Food.kt
+++ food-service/src/main/kotlin/com/example/foodservice/domain/entity/Food.kt
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.ManyToMany
+import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
 
 @Entity
