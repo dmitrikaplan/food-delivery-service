12d5c072310ffb0e704d1f98b9811cc87fa51048
dmitrykap1an <dk475641@gmail.com>
2023-12-09 22:01:51 +0300

    the problem with scanning repositories has been solved

--- food-service/src/main/kotlin/com/example/foodservice/domain/config/ApplicationConfig.kt
+++ food-service/src/main/kotlin/com/example/foodservice/domain/config/ApplicationConfig.kt
@@ -1,9 +1,15 @@
 package com.example.foodservice.domain.config
 
 import com.example.domain.config.DomainConfig
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @Configuration
 @Import(value = [DomainConfig::class])
+@EnableJpaRepositories(value = ["com.example.foodservice"])
+@EntityScan(basePackages = ["com.example.foodservice"])
+@ComponentScan(basePackages = ["com.example.foodservice"])
 class ApplicationConfig
